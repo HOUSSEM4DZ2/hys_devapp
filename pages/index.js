848c8fa4a173891340b627b210ef4cae17b6f89f
HOUSSEM4DZ2 <z.najmou@yahoo.com>
2022-05-30 22:41:12 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to HysApps!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         We develop  <code>the good Stuff !</code>
         </p>
       </main>
 
